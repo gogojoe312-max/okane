3acdaba8f49183535b4d5b16ae48427041e8bf30
gogojoe312-max <gogojoe312@gmail.com>
2026-08-29 18:38:11 +0900

    v1.8: debt tracking + repayment sim + mail diagnostics + wider mail query

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='okane-2026-08-29-i';
+const C='okane-2026-08-29-j';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==C&&caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
